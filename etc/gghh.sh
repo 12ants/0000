77476a68fc2bd9b963ef01097a47860f4d20db2b
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-10-12 14:03:33 +0200

    Update gghh.sh

--- etc/gghh.sh
+++ etc/gghh.sh
@@ -3,9 +3,9 @@ if [ $UID != 0 ]; then echo -e " \n\n\t This script must be run as root... try c
 apt install -y git gh;
 mkdir /home/$SUDO_USER/.config/ -p; mkdir /root/.config/ -p; 
 echo 'github.com:
-	oauth_token: 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
-	user: 12ants
-	git_protocol: ssh
+    user: 12ants
+    oauth_token: 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
+    git_protocol: ssh
 ' > /home/$SUDO_USER/.config/gh/hosts.yml
 chown $SUDO_USER: /home/$SUDO_USER/* -R
 cp /home/$SUDO_USER/.config/gh/hosts.yml /root/.config/gh/
