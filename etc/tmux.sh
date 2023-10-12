0103a84d6e9596eb014a0b327044a644988ce98e
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-10-12 10:11:14 +0200

    Update tmux.sh

--- etc/tmux.sh
+++ etc/tmux.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ##
-sudo apt install -y awk perl sed tmux;
+sudo apt install -y perl sed tmux;
 ##
-cp .tmux.conf /home/$SUDO_USER/.conf/
-cp .tmux.conf.local /home/$SUDO_USER/.conf/
+cp .tmux.conf /home/$SUDO_USER/
+cp .tmux.conf.local /home/$SUDO_USER/
 ## link in root
 ln /home/$SUDO_USER/.conf/.tmux.conf /root/
 ln /home/$SUDO_USER/.conf/.tmux.conf.local /root/
