eb7dc0ff66af86ee518d86038dabead09d44cbd8
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-10-13 00:40:37 +0200

    Update etc/grub.sh

--- etc/grub.sh
+++ etc/grub.sh
@@ -1,25 +1,24 @@
-#!/bin/bash
-## -- [grub.sh] -- [12ants.github.io]
-## -- Script to auto create a nice grub config.
-##
-echo "--  Installing Tard-Grub 2.0..."
-mkdir -p -m 775 /etc/default/grub_old/; cd /etc/default/grub_old/; pwd; ls; 
-mv /etc/default/grub.d/* /etc/default/grub_old/; 
-##
-mkdir -p -m 775 /boot/grub/; cd /boot/grub/;
-wget $blue -O /boot/grub/tard.jpg https://github.com/12ants/0000/raw/main/img/tard.jpg $yellow;
-##
-echo 'GRUB_BACKGROUND="/boot/grub/tard.jpg"
+
+#/bin/bash
+#grub
+mkdir -p -m 775 /boot/grub/ ; 
+cd /boot/grub/ ;
+wget -O tard.jpg https://github.com/12ants/00/raw/main/img/tard2.jpg;
+rm /etc/default/grub.d/*;
+echo '
+GRUB_BACKGROUND="/boot/grub/tard.jpg"
+GRUB_GFXMODE=auto
+GRUB_TERMINAL_OUTPUT="gfxterm"
 GRUB_DEFAULT=saved
 GRUB_SAVEDEFAULT=true
 GRUB_TIMEOUT_STYLE=menu
 GRUB_TIMEOUT=4
-GRUB_DISTRIBUTOR=`lsb_release -as 2> /dev/null || echo Debian`
+GRUB_DISTRIBUTOR=`lsb_release -dcs 2> /dev/null || echo Debian`
 GRUB_DISABLE_OS_PROBER=false
 GRUB_HIDDEN_TIMEOUT=0
 GRUB_CMDLINE_LINUX="console" 
-GRUB_CMDLINE_LINUX_DEFAULT="console" 
-'>etc/default/grub; 
-update-grub; 
-echo -e "$re  \n\n\n\n --$pink GRUB INSTALLED SUCCESSFULLY \n" $re; sleep 2;
-echo -e " $cyan ####################################\n  ####$re -All done! $cyan ###################\n  ####################################\n\n"; sleep 2;
+GRUB_CMDLINE_LINUX_DEFAULT="gfxterm" 
+GRUB_GFXPAYLOAD_LINUX=keep
+' > /etc/default/grub; 
+update-grub ; 
+echo "    OK    "
