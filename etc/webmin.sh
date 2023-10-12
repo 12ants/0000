c978dc80e0b9530c72271aaa94cfbc0b06a4bc07
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-10-12 03:13:08 +0200

    Update webmin.sh

--- etc/webmin.sh
+++ etc/webmin.sh
@@ -3,6 +3,7 @@
 ## WEBMIN        ######
 #######################
 ##
+read -ep "         -- Install WEBMIN? $(tput setaf 2)" -i "y" "webmin";
 if [ $webmin == y ]; then echo "installing webmin";
 wget https://download.webmin.com/devel/tarballs/webmin-current.tar.gz;
 tar -xf webmin-current.tar.gz --strip-components=1; 
