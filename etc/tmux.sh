b6381c0d276416b5576c6971258208300c033f0e
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-10-12 10:20:23 +0200

    Update tmux.sh

--- etc/tmux.sh
+++ etc/tmux.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
+## TMUX and CONFIG
 ##
+if [ $UID != 0 ]; then echo -e " \n\n\t This script must be run as root... try command: [ sudo -s ] \n\n " 1>&2; exit 1; fi; ## ROOT-CHECK
 sudo apt install -y perl sed tmux;
-##
-cp .tmux.conf /home/$SUDO_USER/
-cp .tmux.conf.local /home/$SUDO_USER/
-## link in root
-ln /home/$SUDO_USER/.conf/.tmux.conf /root/
-ln /home/$SUDO_USER/.conf/.tmux.conf.local /root/
+cp .tmux.conf /home/$SUDO_USER/; cp .tmux.conf.local /home/$SUDO_USER/; cp .tmux.conf ~/; cp .tmux.conf.local ~/;
+chown $SUDO_USER: /home/$SUDO_USER/* -R; chmod 775 /home/$SUDO_USER/*; 
+ln /home/$SUDO_USER/.conf/.tmux.conf /root/; ln /home/$SUDO_USER/.conf/.tmux.conf.local /root/
 ##
