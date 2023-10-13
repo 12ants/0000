4a5fd12f0525fa6204591d875bac3db100af56a7
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-10-13 20:40:16 +0200

    Update 0000.sh

--- 0000.sh
+++ 0000.sh
@@ -62,6 +62,24 @@ curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/ke
 echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
 apt update && apt install gum -y;
 curl -sfL https://raw.githubusercontent.com/mistakenelf/fm/main/install.sh | sh;
+
+
+# Debian/Ubuntu
+# melt store ssh pp
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+sudo apt update && sudo apt install melt
+
+
+
+
+echo "deb [signed-by=/usr/share/keyrings/azlux-archive-keyring.gpg] http://packages.azlux.fr/debian/ stable main" | sudo tee /etc/apt/sources.list.d/azlux.list
+sudo wget -O /usr/share/keyrings/azlux-archive-keyring.gpg  https://azlux.fr/repo.gpg
+apt install -y sss duf broot 
+
+
+
 else echo "OK"; fi; cd $inst;
 
 
