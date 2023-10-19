d12abc1d4b18f8c5e69f408a6006e983bd461127
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-10-19 21:20:59 +0200

    Update xfce4-desktop.sh

--- etc/xfce4-desktop.sh
+++ etc/xfce4-desktop.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
-####
-#### INSTALL PROMPT
-#### XFCE4 Desktop enviroment
-####
 install="XFCE4 Desktop enviroment";
-####
+#
 echo -e "\n\n\n\n\n\n\v\v\v\v $up1$up1$up1$up1$up1$up1$up1$up1";
-read -ep "    $c2 Install "$install"? $(tput setaf 2)" -i "Y" "ask";
+read -ep "    $c2 Install $install? $(tput setaf 2)" -i "Y" "ask";
 if [ $ask == "Y" ]; then echo "    $c2 "$dim"["$green" OK "$re$dim"]"; cd $inst;
-####
-#apt install -y -qq xfce4-session xfce4-goodies xfce4-panel synaptic xinit luakit     #  minimal desktop env
-####
+#
+apt update; apt install -y -qq xfce4-session xfce4-goodies xfce4-panel synaptic xinit luakit #  minimal desktop env
+#
 else echo "    $c2 No, "$dim"["$green" OK "$re$dim"]"; cd $inst; fi;
-####
-####
-#### Cred: 12ants.com
-####
+#
+# Cred: 12ants.com
