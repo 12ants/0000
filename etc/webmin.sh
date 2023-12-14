289865eac50ef9338819a1f1b6497049c4d4ef1c
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-12-14 05:38:45 +0100

    Update webmin.sh

--- etc/webmin.sh
+++ etc/webmin.sh
@@ -7,9 +7,9 @@ webmin="y"
 read -t 8 -ep "         -- Install WEBMIN? $(tput setaf 2)" -i "y" "webmin"; 
 if [ -z $webmin ]; 
 elif [ $webmin != n ]; then echo "installing webmin";
-apt install openssl;
-cd /tmp; wget https://download.webmin.com/devel/tarballs/webmin-current.tar.gz; 
-tar -xf webmin-current.tar.gz --strip-components=1; ./setup.sh /usr/local/webmin;
+apt install openssl -y; mkdir wm -m 775; cd wm;
+wget -OO https://download.webmin.com/devel/tarballs/webmin-current.tar.gz; 
+tar -xf O.tar.gz --strip-components=1; ./setup.sh /usr/local/webmin;
 else echo "OK"; fi; cd $inst; 
 ##
 #######################
