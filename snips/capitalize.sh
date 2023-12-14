5c2de2340eeef6657e700b345aedd3edcd975d9d
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-12-14 12:12:16 +0100

    Update capitalize.sh

--- snips/capitalize.sh
+++ snips/capitalize.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-read Cap: -i "$PWD" yno; cd $yno;
+read -ep "$c2 Cap: " -i "$PWD" yno; cd $yno;
 a1=($(ls))
 a2=($(ee ${a1[@]^}))       ## Cap-letter
 a4=($(ee ${a2[@]//_/" "})) ## rm _
 a6=($(ee ${a4[@]%.*}))     ## rm .*
+ee ${a6[@]} 
