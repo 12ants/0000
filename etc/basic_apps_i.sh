aabe84a5aaa49db906b9747ffb82d6f87630a0f6
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-12-15 10:36:34 +0100

    Update basic_apps_i.sh

--- etc/basic_apps_i.sh
+++ etc/basic_apps_i.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-# BASIC APPS
+# BASIC APPS #
+##############
+
 tput cuu 8; tput ed; ee -e "\n\n$c2 Installing basic apps . . . \n\n";
 apt update; apt upgrade -y; apt install -y gpm links2 ranger mc curl lolcat googler 2>/dev/null ; tput cuu 8; tput ed; ee -e "\n\n$c2 Basic apps installed . . . \n\n";
-#
+#### nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+echo "$c2 OK"
+
+##
+##
+##
+##
