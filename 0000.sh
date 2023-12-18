5579173904f4f7bd232ee5c641a869e9d7bdde42
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-12-18 15:47:51 +0100

    wefet

--- 0000.sh
+++ 0000.sh
@@ -34,22 +34,18 @@ chsh -s /bin/bash 12ants
 ##
 ##
 ##
-
-
-
 #read -ep "$c2 Cap: " -i "$PWD" yno; cd $yno;
+cd $ghh/etc;
 a1=($(ls))
 a2=($(ee ${a1[@]^}))       ## Cap-letter
 a4=($(ee ${a2[@]//_/" '"})) ## rm _
 a6=($(ee ${a4[@]%.*}))     ## rm .*
 ee ${a6[@]} 
 ops=(${a6[@]})
-
-
-
-a1=($(ls)); a2=(${a1[@]^}); a4=(${a2[@]//_/"   " }); a6=(${a4[@]//.*/  }); 
-echo o 
-{a6[@]}
+. multi.sh
+#a1=($(ls)); a2=(${a1[@]^}); a4=(${a2[@]//_/"   " }); a6=(${a4[@]//.*/  }); 
+#echo o 
+#{a6[@]}
 
 
 
