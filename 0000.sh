1deeb5a63e9b21406f41c2b6c0708e7f95ba1867
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-12-18 08:15:31 +0100

    Update 0000.sh

--- 0000.sh
+++ 0000.sh
@@ -1,30 +1,29 @@
-#!/bin/bash ############################
-echo '   =================================
-##      wget -OOK 12ants.github.io; bash OK
-##       =================================
-##      BASH-IMPORVEMENTS Install-script for
-##      Ubuntu/Debian systems by -- 12ants.github.io
-## ' #################################################
-if [ $UID != 0 ]; then echo -e " \n\n\t This script must be run as root... try command: [ sudo -s ] \n\n " 1>&2; exit 1; fi
+#!/bin/bash 
+############# 12ants.github.com
+clear 
+if [ $UID != 0 ]; then echo -e " \n\n\t This script must be run as root... try command: [ sudo -s ] \n\n " 1>&2; exit 1; fi 
 ## Autorestart as needed
-sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf; clear;
-snap install lolcat 2>/dev/null
-apt update|lolcat -F .01 -s 111 -p 11 -d 22; apt upgrade -y --install-recommends|lolcat -F .001 -s 111 -p 11 -d 22;
+sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf; 
+# snap install lolcat 2>/dev/null
+# apt update|lolcat -F .01 -s 111 -p 11 -d 22; apt upgrade -y --install-recommends|lolcat -F .001 -s 111 -p 11 -d 22;
 ##
 ## ADDING COLOR-CODES -- (Need to run inside other command.)
+
 export bold=$(tput bold) dim=$(tput dim) so=$(tput smso) noso=$(tput rmso) rev=$(tput rev) re=$(tput sgr0) normal=$(tput sgr0) \
 redb=$(tput setab 1) greenb=$(tput setab 2) yellowb=$(tput setab 3) blueb=$(tput setab 4) purpleb=$(tput setab 5) cyanb=$(tput setab 6) \
 grayb=$(tput setab 7) red=$(tput setaf 1) green=$(tput setaf 2) yellow=$(tput setaf 3) blue=$(tput setaf 4) purple=$(tput setaf 5) \
 cyan=$(tput setaf 6) gray=$(tput setaf 7) white=$(tput setaf 7 bold) pink=$(tput setaf 5 bold) darkblue=$(tput setab 5 bold) blink=$(tput blink) \
-left2=$(tput cub 2) up1=$(tput cuu1) c75="  ----------------"; clear; echo ; c2="$cyan --$re";
+left2=$(tput cub 2) up1=$(tput cuu1) c35=""$cyan"--------------------------------$re" c2="$cyan --$re";
+clear; 
+echo -e " \n\n\n\n\t\t $c35\n\t\t Welcome to$cyan 12ants$re bash-improver! \n\t\t $c35\n\n"; sleep 2; 
 export own="chown "$SUDO_USER": /home/"$SUDO_USER" -R";
 ##
 ##
 ##
-####
-echo -e "\n\n\n\n\t
- -- 0000_bash_improvements... \n\n\n\n\n\n\n"; tput sgr0;
-tput cuu1; tput cuu1; tput cuu1; tput cuu1; tput cuu1; tput cuu1; tput cuu1;
+# ####
+# echo -e "\n\n\n\n\t
+#  -- 0000_bash_improvements... \n\n\n\n\n\n\n"; tput sgr0;
+# tput cuu1; tput cuu1; tput cuu1; tput cuu1; tput cuu1; tput cuu1; tput cuu1;
 
 
 
